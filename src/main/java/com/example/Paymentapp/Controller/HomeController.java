3e48b50976cfbdfd16a9c7d30cd42230b02c39ed
Tadimeti Venkata Sri harshita <harshitatadimeti2002@gmail.com>
2025-04-14 22:36:05 +0530

    Updated HomeController.java

--- src/main/java/com/example/Paymentapp/Controller/HomeController.java
+++ src/main/java/com/example/Paymentapp/Controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.Paymentapp.Controller;
+package com.example.PaymentsWebApp.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,33 +6,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/index")
-    public String homePage() {
-        return "index";  // Renders home.jsp
-    }
+	@GetMapping("/")
+	public String landingPage() {
+		return "landing";
+	}
 
-    @GetMapping("/profile")
-    public String profilePage() {
-        return "profile";  // Renders profile.jsp
-    }
+	@GetMapping("/index")
+	public String homePage() {
+		return "index"; // Renders home.jsp
+	}
 
-    @GetMapping("/transactions")
-    public String transactionsPage() {
-        return "transactions";  // Renders transactions.jsp
-    }
+//    @GetMapping("/profile")
+//    public String profilePage() {
+//        return "profile";  // Renders profile.jsp
+//    }
 
-    @GetMapping("/bankstatement")
-    public String statementPage() {
-        return "bankstatement";  // Renders statement.jsp
-    }
-    
-    @GetMapping("/logout")
-    public String logoutPage() {
-        return "logout"; // Renders logout.jsp
-    }
-    
-    @GetMapping("/dashboard")
-    public String dashboardPage() {
-        return "dashboard"; // Renders dashboard.jsp
-    }   
+//	@GetMapping("/viewtransactions")
+//	public String transactionsPage() {
+//		return "viewtransactions"; // Renders viewtransactions.jsp
+//	}
+
+	@GetMapping("/bankstatement")
+	public String statementPage() {
+		return "bankstatement"; // Renders bankstatement.jsp
+	}
+
+//	@GetMapping("/logout")
+//	public String logoutPage() {
+//		return "logout"; // Renders logout.jsp
+//	}
 }
